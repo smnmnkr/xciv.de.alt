ff33d0184d50d179cc070ccdf397b1581da7c79a
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-12-01 22:28:30 +0100

    added matcher for queries

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,22 @@ module.exports = {
     require("tailwindcss")("./tailwind.config.js"),
     require("autoprefixer")({ path: "./" }),
     ...(process.env.HUGO_ENVIRONMENT === "production"
-      ? [require("@fullhuman/postcss-purgecss")({ content: ["./**/*.html"] })]
+      ? [
+          require("@fullhuman/postcss-purgecss")({
+            content: ["./**/*.html"],
+            defaultExtractor: (content) => {
+              // Capture as liberally as possible, including things like `h-(screen-1.5)`
+              const broadMatches =
+                content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+              // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+              const innerMatches =
+                content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+              return broadMatches.concat(innerMatches);
+            },
+          }),
+        ]
       : []),
   ],
 };
